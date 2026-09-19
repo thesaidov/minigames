711acf5b8f9f754c9c53d65ac9a82656d7546e3b
thesaidov <sardorbekkamoliddinovich@gmail.com>
2026-09-20 01:31:13 +0500

    eslint configuration updated

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,20 +1,15 @@
-<<<<<<< Updated upstream
-=======
 import "../styles/globals.scss";
 
->>>>>>> Stashed changes
 const app = document.querySelector<HTMLDivElement>("#app");
 
 if (app) {
   app.innerHTML = `
-<<<<<<< Updated upstream
+
     <h1>MiniGames</h1>
     <p>Vite is working!</p>
-=======
     <main>
       <h1>MiniGames</h1>
       <p>Welcome to MiniGames!</p>
     </main>
->>>>>>> Stashed changes
   `;
 }
