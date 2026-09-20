53ba1a47177711934710a25ee00a677ed3a7b3d9
thesaidov <sardorbekkamoliddinovich@gmail.com>
2026-09-20 11:46:33 +0500

    prettier configured

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,32 @@
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import unicorn from "eslint-plugin-unicorn";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
 
 export default defineConfig([
   {
-    ignores: ["dist/**", "node_modules/**"],
+    ignores: ['dist/**', 'node_modules/**'],
   },
 
   {
-    files: ["**/*.{js,ts,mjs,cjs,mts,cts}"],
+    files: ['**/*.{js,ts,mjs,cjs,mts,cts}'],
 
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      unicorn.configs["recommended"],
-    ],
+    extends: [js.configs.recommended, tseslint.configs.recommended, unicorn.configs['recommended']],
 
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
     },
 
     linterOptions: {
       noInlineConfig: true,
-      reportUnusedDisableDirectives: "error",
+      reportUnusedDisableDirectives: 'error',
     },
 
     rules: {
-      "@typescript-eslint/no-explicit-any": "error",
+      '@typescript-eslint/no-explicit-any': 'error',
     },
   },
 ]);
